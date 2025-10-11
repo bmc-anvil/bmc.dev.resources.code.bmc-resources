ab7078cf7a526e35c1da1a0e4bc9f47c52a11ac5
xavier <6283405+JohnnyXavier@users.noreply.github.com>
2025-10-11 21:57:30 +0200

    - removing unused functionality

--- src/main/java/bmc/dev/resources/code/bmcresources/maven/MavenConfigFileUtils.java
+++ src/main/java/bmc/dev/resources/code/bmcresources/maven/MavenConfigFileUtils.java
@@ -3,12 +3,9 @@ package bmc.dev.resources.code.bmcresources.maven;
 import java.util.List;
 import java.util.OptionalInt;
 import java.util.function.BiFunction;
-import java.util.function.Predicate;
 
 import lombok.experimental.UtilityClass;
 
-import static java.lang.Boolean.FALSE;
-import static java.lang.System.getProperty;
 import static java.util.Optional.ofNullable;
 import static java.util.OptionalInt.empty;
 import static java.util.stream.IntStream.range;
@@ -42,12 +39,5 @@ public class MavenConfigFileUtils {
                                          .filter(i -> strings.get(i).contains(property))
                                          .findFirst())
                     .orElse(empty());
-
-    /**
-     * {@link Predicate} to evaluate if a given Maven property is both set and has a value of {@code true}.
-     * <p>
-     * If the property exists AND its value is {@code true}, the predicate returns {@code true}; otherwise, it returns {@code false}.
-     */
-    public static final Predicate<String> isPropertySetAndTrue = property -> ofNullable(getProperty(property)).map(Boolean::valueOf).orElse(FALSE);
 
 }
